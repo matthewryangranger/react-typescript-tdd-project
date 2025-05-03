f8e707ab9f874bacea117d301af24d760970c411
Matthew Ryan Granger <mattg1827@gmail.com>
2025-05-03 00:11:39 -0700

    @ r Rename App to MyApp

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,9 +1,9 @@
-import React from 'react';
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import React from "react";
+import { render, screen } from "@testing-library/react";
+import MyApp from "./MyApp";
 
-test('renders learn react link', () => {
-  render(<App />);
+test("renders learn react link", () => {
+  render(<MyApp />);
   const linkElement = screen.getByText(/learn react/i);
   expect(linkElement).toBeInTheDocument();
 });
