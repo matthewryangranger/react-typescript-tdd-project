ae86a5bcbdf8f236ce1cd00c8527ab0abc31f890
Matthew Ryan Granger <mattg1827@gmail.com>
2025-05-03 23:59:42 -0700

    @ f Make first test pass

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -2,8 +2,8 @@ import React from "react";
 import { render, screen } from "@testing-library/react";
 import MyApp from "./MyApp";
 
-test("renders learn react link", () => {
+test("renders hello react", () => {
   render(<MyApp />);
-  const linkElement = screen.getByText(/learn react/i);
+  const linkElement = screen.getByText(/Hello, React/i);
   expect(linkElement).toBeInTheDocument();
 });
