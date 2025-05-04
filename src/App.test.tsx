ee15c4d9f11f1546f60a26c6db588e08db708d7e
Matthew Ryan Granger <mattg1827@gmail.com>
2025-05-04 00:51:20 -0700

    @ r Revert test cases to only one test

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,14 +1,9 @@
 import React from "react";
 import { render, screen } from "@testing-library/react";
-import MyApp, { label } from "./MyApp";
+import MyApp from "./MyApp";
 
 test("renders hello react", () => {
   render(<MyApp />);
   const greeting = screen.getByText(/Hello, React/i);
   expect(greeting).toBeInTheDocument();
-});
-
-test("generates a label", () => {
-  const result = label("React");
-  expect(result).toEqual("Hello, REACT");
 });
--- src/MyApp.tsx
+++ src/MyApp.tsx
@@ -1,13 +1,9 @@
 import React from "react";
 
-export function label(text: string) {
-  return `Hello, ${text.toUpperCase()}`;
-}
-
 function MyApp() {
   return (
     <div>
-      <h1>{label("React")}</h1>
+      <h1>"Hello, React!"</h1>
     </div>
   );
 }
